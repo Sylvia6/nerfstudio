0e87bc449a15f774f2fef6f36f70ebcad5222cd8
Sylvia6 <1505723831@qq.com>
2023-08-23 02:54:19 +0000

    fix obj pose

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -85,18 +85,19 @@ method_configs["nerfacto"] = TrainerConfig(
     method_name="nerfacto",
     steps_per_eval_batch=500,
     steps_per_save=2000,
-    max_num_iterations=30000,
+    max_num_iterations=60000,
     mixed_precision=True,
     pipeline=VanillaPipelineConfig(
         datamanager=VanillaDataManagerConfig(
             dataparser=NerfstudioDataParserConfig(),
             train_num_rays_per_batch=4096,
             eval_num_rays_per_batch=4096,
-            camera_optimizer=CameraOptimizerConfig(
-                mode="SO3xR3",
-                optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
-                scheduler=ExponentialDecaySchedulerConfig(lr_final=6e-6, max_steps=200000),
-            ),
+            camera_optimizer=CameraOptimizerConfig(mode="off"),
+            # camera_optimizer=CameraOptimizerConfig(
+            #     mode="SO3xR3",
+            #     optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
+            #     scheduler=ExponentialDecaySchedulerConfig(lr_final=6e-6, max_steps=200000),
+            # ),
         ),
         model=NerfactoModelConfig(eval_num_rays_per_chunk=1 << 15),
     ),
